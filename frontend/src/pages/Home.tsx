b707daeb27774923dfc542b4c0b0e257db42bf5e
YJ-0220 <sneakers1577@gmail.com>
2025-09-26 17:03:10 +0900

    오류 해결

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import BannerSlider from "@/components/section/BannerSlider";
+import EventBanner from "@/components/section/EventBanner";
 import CategoryNav from "@/components/section/CategoryNav";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       {/* 배너 슬라이드 섹션 */}
-      <BannerSlider />
+      <EventBanner />
       {/* 카테고리 네비게이션 섹션 */}
       <CategoryNav />
     </div>
